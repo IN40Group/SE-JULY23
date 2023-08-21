ecd56ee6acd15710c1566a936192b58254c3a0ea
Amira Ahmed Trainer <97535863+Amytrainer@users.noreply.github.com>
2023-08-21 10:41:58 +0100

    Add files via upload

---
+++ src/component/Basics/Resturant.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./style.css";
+import Menu from "./menuApi";
+import MenuCard from "./MenuCard";
+import Navbar from "./Navbar"
+
+const uniqueList = [
+  ...new Set(Menu.map((loopcard) => {
+    return loopcard.category;
+  })), "All",
+
+];
+
+console.log(uniqueList);
+
+
+function Resturant() {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList)
+  
+  const filterItem = (category) => {
+    if(category==="All"){setMenuData(Menu);
+    return;
+    }
+    const updatedList = Menu.filter((loopcard) => {
+      return loopcard.category === category;
+    }
+
+    );
+    setMenuData(updatedList);
+  };
+  return (
+    <>
+      {/* <Navbar filterItem={filterItem}
+        menuList={menuList} /> */}
+      <MenuCard
+        menuData={menuData}
+      />
+
+    </>
+  );
+};
+
+export default Resturant;
